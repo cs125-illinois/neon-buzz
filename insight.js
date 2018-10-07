bdb0414f03da4a3998d28f505d6e1676a2b46ab7
Sathwik Pochampally <sathwik@outlook.com>
2018-10-06 19:27:42 -0500

    Added buzz function

--- insight.js
+++ insight.js
@@ -5,7 +5,10 @@ function foundDevice(err, device) {
   if (device.deviceType === Wemo.DEVICE_TYPE.Insight) {
     console.log('Wemo Insight Switch found: %s', device.friendlyName);
 
+    // Keeping track of Switch's state
+    var state = 'off';
     var client = this.client(device);
+
     client.on('insightParams', function(state, power) {
       console.log('%s’s power consumption: %s W',
         this.device.friendlyName,
@@ -13,6 +16,14 @@ function foundDevice(err, device) {
       );
     });
   }
+}
+
+function buzz(device) {
+  var client = this.client(device);
+  setInterval(function() {
+      client.setBinaryState(1);
+      }, 5000);
+  client.setBinaryState(0);
 }
 
 wemo.discover(foundDevice);
